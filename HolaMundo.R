c7f434207c03cdc16199dbb974a92121849380d8
JuanJimenezGarcia <juanjimenezgarcia5@gmail.com>
2020-12-05 18:18:14 +0100

    Hemos modificado el script HolaMundo, añadiendo la variable adios_mundo

--- HolaMundo.R
+++ HolaMundo.R
@@ -5,3 +5,6 @@
 
 hola_mundo <- "ESTE ES MI PRIMER PROYECTO CON CONTROL DE VERSIONES GIT"
 print(hola_mundo)
+
+adios_mundo <- "ESTE ES MI SEGUNDO STRING CON CONTROL DE VERSIONES GIT"
+print(adios_mundo)
